fbfc56a0ed9f8258ef1493a932d01525c3464b3a
vyas-lab <svyas.github@gmail.com>
2025-08-17 00:01:46 +0000

    Deploying to gh-pages from @ vyas-lab/vyas-lab.github.io@b5fd024ba25ff62f3e17a0b8b11f72bf79aa56bd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Courses I am currently teaching at UCF.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
